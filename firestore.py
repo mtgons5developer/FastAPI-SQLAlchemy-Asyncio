6e18e57afd005d40c8942d416a6bb5f771c649b7
Ernesto H. Almeda Jr <mtgons5.ea@gmail.com>
2023-09-10 03:41:29 +0800

    Update firestore.py

--- firestore.py
+++ firestore.py
@@ -1,53 +1,46 @@
 from fastapi import FastAPI
-from async_firestore_sqlalchemy import AsyncFirestoreSQLAlchemy
-import asyncio
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Firebase Admin SDK with your service account key JSON file
+cred = credentials.Certificate("your_firestore_credentials.json")  # Replace with your Firestore credentials JSON file
+firebase_admin.initialize_app(cred)
 
 app = FastAPI()
 
-# SQLAlchemy Configuration
-db = AsyncFirestoreSQLAlchemy(
-    database_url='your_firestore_credentials.json',  # Replace with your Firestore credentials JSON file
-    echo=True  # Set to True for debugging, False for production
-)
-
-# SQLAlchemy Model
-class Item(db.Model):
-    __tablename__ = 'items'
-    id = db.Column(db.String, primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String)
-
-# Create database tables
-@app.on_event("startup")
-async def startup_db():
-    await db.connect()
-
-@app.on_event("shutdown")
-async def shutdown_db():
-    await db.disconnect()
-
 # FastAPI Endpoints
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item):
-    async with db.transaction():
-        new_item = Item(**item.dict())
-        db.session.add(new_item)
-        await db.session.commit()
-        await db.session.refresh(new_item)
-        return new_item
+@app.post("/items/")
+async def create_item(item: dict):
+    db = firestore.client()  # Initialize Firestore client
+    try:
+        doc_ref = db.collection("locations").add(item)
+        return {"message": "Item created successfully", "id": doc_ref.id}
+    except Exception as e:
+        return {"error": str(e)}
 
-@app.get("/items/{item_id}", response_model=Item)
+@app.get("/items/{item_id}")
 async def read_item(item_id: str):
-    item = await db.session.query(Item).filter_by(id=item_id).first()
-    if item:
-        return item
-    else:
-        return {"error": "Item not found"}
+    db = firestore.client()  # Initialize Firestore client
+    try:
+        doc_ref = db.collection("locations").document(item_id)
+        item = doc_ref.get()
+        if item.exists:
+            item_data = {"id": item.id, **item.to_dict()}
+            return item_data
+        else:
+            return {"error": "Item not found"}
+    except Exception as e:
+        return {"error": str(e)}
 
-@app.get("/items/", response_model=list[Item])
+@app.get("/items/")
 async def read_items():
-    items = await db.session.query(Item).all()
-    return items
+    db = firestore.client()  # Initialize Firestore client
+    try:
+        items = db.collection("locations").stream()
+        item_list = [{"id": item.id, **item.to_dict()} for item in items]
+        return item_list
+    except Exception as e:
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
